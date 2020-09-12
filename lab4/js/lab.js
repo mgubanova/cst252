63ab33693428c2ad643b46cc366f055d6de02b57
mgubanova <70294900+mgubanova@users.noreply.github.com>
2020-09-12 14:48:06 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Name: Chess Game Pseudocode
+// Authors: Sebastian Tamiano, Marta Gubanova
+
+// Print the board and pieces before each game
+// Prompt user input
+// Receive user input
+// Validate the input
+    // Check the piece and its parameters
+    // Compare to the piece moving rules
+//  Accept or deny the input
+    // Send input data to the opponent
+    // Update printed cells to reflect the new positions of the chess piece
+    // Test for win condition
+// Repeat steps 2-5 until win condition is met
+// Prompt the winning user side of the board with confetti!
