5fcf34f0a650020d5dacedfa8daccde572a243d3
mgubanova <70294900+mgubanova@users.noreply.github.com>
2020-09-15 23:35:48 -0700

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+  * Author: Marta Gubanova
+  * Partner: Sebastian Tamiano
+  * Created: 15 September 2020
+  * License: Public Domain
+*/
+// Define variables
+var make = "Honda";
+var model = "Civic";
+var color = "Grey";
+var ownIt = true;
+var year = 2007;
+var currentYear = 2020;
+
+// Calculate age
+age = currentYear - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years <br>");
+document.writeln("It is " + ownIt + " that I own it<br>");
